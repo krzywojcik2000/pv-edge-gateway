fd6a7c795f208fceddcc9a43496385526d0c58b3
krzywojcik2000 <krzy.wojcik2000@gmail.com>
2026-07-06 19:16:22 +0200

    refactor(runtime): move system startup responsibility to main.py as single source of truth

--- edge_gateway/main.py
+++ edge_gateway/main.py
@@ -1,70 +1,15 @@
-import time
-
-from modbus_reader import ModbusReader
-from modbus_decoder import ModbusDecoder
-from database_writer import DatabaseWriter
-from mqtt_publisher import MQTTPublisher
-from opcua_server import OPCUAServer
+from edge_gateway.runtime.application import Application
+from edge_gateway.config.config_loader import load_config
 
 
-# -----------------------------
-# CONFIG
-# -----------------------------
-config = {
-    "db_name": "pv_scada",
-    "db_user": "pv_user",
-    "db_password": "pv_pass",
-    "db_host": "localhost",
-    "db_port": 5432
-}
+def main():
 
-FARM_ID = "KRK-01"
+    config = load_config()
+
+    app = Application(config)
+    app.build()
+    app.run()
 
 
-# -----------------------------
-# INIT LAYERS
-# -----------------------------
-reader = ModbusReader(host="127.0.0.1", port=5020)
-decoder = ModbusDecoder()
-db = DatabaseWriter(config)
-mqtt = MQTTPublisher()
-opcua = OPCUAServer()
-
-
-# START OPC UA SERVER (IMPORTANT)
-opcua.start()
-
-print("[EDGE] Gateway started...")
-
-
-# -----------------------------
-# MAIN LOOP
-# -----------------------------
-while True:
-    raw = reader.read_raw()
-
-    if raw:
-        data = decoder.decode(raw)
-
-        if data:
-            # -------------------------
-            # STORAGE
-            # -------------------------
-            db.insert_raw(FARM_ID, data)
-            db.insert_alarm(FARM_ID, data)
-            db.insert_business(FARM_ID, data)
-
-            # -------------------------
-            # MQTT STREAMING
-            # -------------------------
-            mqtt.publish_telemetry(FARM_ID, data)
-            mqtt.publish_alarm(FARM_ID, data)
-
-            # -------------------------
-            # OPC UA (SCADA LAYER)
-            # -------------------------
-            opcua.update(data)
-
-            print(f"[OK] full pipeline executed: {data}")
-
-    time.sleep(1)
+if __name__ == "__main__":
+    main()
